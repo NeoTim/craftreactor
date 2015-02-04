2b64865e9c263e1d8943764899d5279178c718e0
joelcoxokc <joelcox@hisimagination.com>
2015-02-04 02:20:02 -0800

    UPdate

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -5,8 +5,9 @@
 
   module.exports = function(app, config) {
     // app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
-    app.use(express.static(path.join(config.root, '/client')));
-    app.set('appPath', config.root + '/client');
+    app.use(express.static(path.join(config.root, '/build')));
+    app.set('env', 'production');
+    app.set('appPath', path.join(config.root + '/build') );
   };
 
 })();
